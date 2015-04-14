612919c4f3dacfdf2610c8dd88b47d7938834835
Ealden Esto E. Escañan <ealden@dot.ph>
2015-04-14 13:45:08 +0800

    Removed bin/ from linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :rails_env, 'production'
 set :deploy_to, '/srv/cocca-api'
 set :log_level, :info
 set :linked_files, %w{config/secrets.yml config/database.yml config/exception_notification.yml config/registry.yml config/resque.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :default_env, { path: "$PATH:/usr/pgsql-9.3/bin" }
 
