d02301968c6805bed0c703e124e01b1d3fb8723b
Ealden Esto E. Escañan <ealden@dot.ph>
2015-07-06 11:07:52 +0800

    Return period in years

--- app/models/audit/domain_event.rb
+++ app/models/audit/domain_event.rb
@@ -1,3 +1,11 @@
 class Audit::DomainEvent < ActiveRecord::Base
   self.table_name = :audit_domain_event
+
+  def period
+    self.in_years? ? self.term_length : (self.term_length / 12)
+  end
+
+  def in_years?
+    self.term_units == 'YEAR'
+  end
 end
