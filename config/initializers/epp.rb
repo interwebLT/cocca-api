0f8d5864e3e236a9924491c2a2a5cb449b4db5ab
Ealden Esto E. Escañan <ealden@dot.ph>
2015-08-28 16:05:58 +0800

    Should be in epp and not registry

--- config/initializers/epp.rb
+++ config/initializers/epp.rb
@@ -1,5 +1,7 @@
 config = Rails.application.config_for(:epp).with_indifferent_access
 
+require 'epp-client'
+
 Rails.configuration.x.epp_host  = config[:host]
 Rails.configuration.x.epp_username  = config[:username]
 Rails.configuration.x.epp_password  = config[:password]
