4061d76dd21f0d3dca19eeb76c6e7cfb2d37066f
Ealden Esto E. Escañan <ealden@dot.ph>
2015-09-04 15:11:13 +0800

    Initial Contact model

---
+++ app/models/contact.rb
@@ -0,0 +1,15 @@
+class Contact
+  include ActiveModel::Model
+  include ActiveModel::Validations
+
+  attr_accessor :handle, :name, :street, :city, :country_code, :voice, :email, :authcode
+
+  validates :handle,  presence: true
+  validates :name,    presence: true
+  validates :street,  presence: true
+  validates :city,    presence: true
+  validates :country_code,  presence: true
+  validates :voice,   presence: true
+  validates :email,   presence: true
+  validates :authcode,  presence: true
+end
---
+++ test/models/contact_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+describe Contact do
+  describe :valid? do
+    subject { Contact.new params }
+
+    let(:params) {
+      {
+        handle: 'contact123',
+        name: 'Contact',
+        street: 'Street',
+        city: 'City',
+        country_code: 'PH',
+        voice:  '+63.1234567',
+        email:  'contact@test.ph',
+        authcode: 'ABC123'
+      }
+    }
+
+    specify { subject.valid?.must_equal true }
+    specify { subject.handle = nil; subject.valid?.must_equal false }
+    specify { subject.name= nil;    subject.valid?.must_equal false }
+    specify { subject.street = nil; subject.valid?.must_equal false }
+    specify { subject.city = nil;   subject.valid?.must_equal false }
+    specify { subject.country_code = nil; subject.valid?.must_equal false }
+    specify { subject.voice = nil;  subject.valid?.must_equal false }
+    specify { subject.email = nil;  subject.valid?.must_equal false }
+    specify { subject.authcode= nil;  subject.valid?.must_equal false }
+  end
+end
