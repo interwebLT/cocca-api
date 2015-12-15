ecbeeee9a2eeaf3b92aff5229c235169dfb1ca09
Ealden Esto E. Escañan <ealden@dot.ph>
2015-12-15 17:45:23 +0800

    Split audit_ledger_factory from common_factory

--- test/factories/common_factory.rb
+++ test/factories/common_factory.rb
@@ -10,31 +10,6 @@ FactoryGirl.define do
     audit_time Time.now
     audit_ip '127.0.0.1'
   end
-
-  factory :audit_ledger, class: Audit::Ledger do
-    audit_transaction
-    audit_operation 'I'
-    sequence(:id, 1) { |id| id}
-    client_roid '3-CoCCA'
-    description 'this is a test'
-    currency 'USD'
-    total 0.00
-    created Time.now
-    balance 0.00
-    tld 'ph'
-
-    factory :transfer_ledger, class: Audit::Ledger do
-      trans_type Audit::Ledger::TRANSFER
-    end
-
-    factory :renew_ledger, class: Audit::Ledger do
-      trans_type Audit::Ledger::RENEW
-    end
-
-    factory :register_ledger, class: Audit::Ledger do
-      trans_type Audit::Ledger::REGISTER
-    end
-  end
 end
 
 def audit_master audit_time, partner: 'alpha'
