539e69ebce2adf64cce1fe9c05c538016884a88e
Ealden Esto E. Escañan <ealden@dot.ph>
2016-01-04 11:23:40 +0800

    Moved renew domain assets into order

--- features/step_definitions/renew_domain_steps.rb
+++ features/step_definitions/renew_domain_steps.rb
@@ -1,37 +1,37 @@
 When /^I renew a domain that exists$/ do
-  client.expect :create, 'domain/renew_response'.epp, [EPP::Domain::Renew]
+  client.expect :create, 'order/create_renew_domain_response'.epp, [EPP::Domain::Renew]
 
   EPP::Client.stub :new, client do
-    post orders_path, 'domain/renew_request'.json
+    post orders_path, 'order/create_renew_domain_request'.json
   end
 end
 
 When /^I renew a domain that is still available$/ do
-  client.expect :create, 'domain/renew_response_failed'.epp, [EPP::Domain::Renew]
+  client.expect :create, 'order/create_renew_domain_failed_response'.epp, [EPP::Domain::Renew]
 
   EPP::Client.stub :new, client do
-    post orders_path, 'domain/renew_request'.json
+    post orders_path, 'order/create_renew_domain_request'.json
   end
 end
 
 Then /^domain must be renewed$/ do
-  json_response.must_equal 'domain/renew_response'.json
+  json_response.must_equal 'order/create_renew_domain_response'.json
 end
 
 When /^I renew a domain with no domain name$/ do
-  request = 'domain/renew_request'.json
+  request = 'order/create_renew_domain_request'.json
   request[:order_details][0][:domain] = ''
   post orders_path, request
 end
 
 When /^I renew a domain with no period$/ do
-  request = 'domain/renew_request'.json
+  request = 'order/create_renew_domain_request'.json
   request[:order_details][0][:domain] = 'domainph'
   post orders_path, request
 end
 
 When /^I renew a domain with period more than (\d+) years$/ do |period|
-  request = 'domain/renew_request'.json
+  request = 'order/create_renew_domain_request'.json
   request[:order_details][0][:period] = (period.to_i + 1).to_s
   post orders_path, request
 end
