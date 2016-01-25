26563513ebcfa8fe5bd39a86af371b64740bc1d3
Ealden Esto E. Escañan <ealden@dot.ph>
2016-01-25 19:24:54 +0800

    Renamed create_ to post_

--- features/step_definitions/renew_domain_steps.rb
+++ features/step_definitions/renew_domain_steps.rb
@@ -1,35 +1,35 @@
 When /^I renew a domain that exists$/ do
-  client.expect :renew, 'order/create_renew_domain_response'.epp, [EPP::Domain::Renew]
+  client.expect :renew, 'order/post_renew_domain_response'.epp, [EPP::Domain::Renew]
 
   EPP::Client.stub :new, client do
-    post orders_path, 'order/create_renew_domain_request'.json
+    post orders_path, 'order/post_renew_domain_request'.json
   end
 end
 
 When /^I renew a domain that is still available$/ do
-  client.expect :renew, 'order/create_renew_domain_failed_response'.epp, [EPP::Domain::Renew]
+  client.expect :renew, 'order/post_renew_domain_failed_response'.epp, [EPP::Domain::Renew]
 
   EPP::Client.stub :new, client do
-    post orders_path, 'order/create_renew_domain_request'.json
+    post orders_path, 'order/post_renew_domain_request'.json
   end
 end
 
 Then /^domain must be renewed$/ do
-  json_response.must_equal 'order/create_renew_domain_response'.json
+  json_response.must_equal 'order/post_renew_domain_response'.json
 end
 
 When /^I renew a domain with no domain name$/ do
-  post orders_path, 'order/create_renew_domain_with_no_domain_name_request'.json
+  post orders_path, 'order/post_renew_domain_with_no_domain_name_request'.json
 end
 
 When /^I renew a domain with no period$/ do
-  post orders_path, 'order/create_renew_domain_with_no_period_request'.json
+  post orders_path, 'order/post_renew_domain_with_no_period_request'.json
 end
 
 When /^I renew a domain with period more than 10 years$/ do
-  post orders_path, 'order/create_renew_domain_with_period_more_than_ten_years_request'.json
+  post orders_path, 'order/post_renew_domain_with_period_more_than_ten_years_request'.json
 end
 
 When /^I renew a domain with no current expires at$/ do
-  post orders_path, 'order/create_renew_domain_with_no_current_expires_at_request'.json
+  post orders_path, 'order/post_renew_domain_with_no_current_expires_at_request'.json
 end
