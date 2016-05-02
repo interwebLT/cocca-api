685a97b3ef1d545510396d33af5d36dc33878e3b
Ealden Esto E. Escañan <ealden@dot.ph>
2016-05-02 14:31:43 +0800

    Moved Contact#as_json specs to RSpec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -86,6 +86,92 @@ RSpec.describe Contact do
     end
   end
 
+  describe '#as_json' do
+    subject do
+      contact = Contact.new partner:            partner,
+                            handle:             handle,
+                            local_name:         local_name,
+                            local_street:       local_street,
+                            local_city:         local_city,
+                            local_country_code: local_country_code,
+                            voice:              voice,
+                            email:              email,
+
+                            local_organization: local_organization,
+                            local_street2:      local_street2,
+                            local_street3:      local_street3,
+                            local_state:        local_state,
+                            local_postal_code:  local_postal_code,
+
+                            voice_ext:          voice_ext,
+                            fax:                fax,
+                            fax_ext:            fax_ext,
+
+                            name:               name,
+                            organization:       organization,
+                            street:             street,
+                            street2:            street2,
+                            street3:            street3,
+                            city:               city,
+                            state:              state,
+                            postal_code:        postal_code,
+                            country_code:       country_code
+
+      contact.as_json
+    end
+
+    let(:local_organization)  { 'Local Organization' }
+    let(:local_street2)       { 'Local Street 2' }
+    let(:local_street3)       { 'Local Street 3' }
+    let(:local_state)         { 'Local State' }
+    let(:local_postal_code)   { 1235 }
+
+    let(:voice_ext)           { 1234 }
+    let(:fax)                 { '+63.1234568' }
+    let(:fax_ext)             { 1235 }
+
+    let(:name)                { 'Contact' }
+    let(:organization)        { 'Organization' }
+    let(:street)              { 'Street' }
+    let(:street2)             { 'Street 2' }
+    let(:street3)             { 'Street 3' }
+    let(:city)                { 'City' }
+    let(:state)               { 'State' }
+    let(:postal_code)         { 1234 }
+    let(:country_code)        { 'PH' }
+
+    let(:expected_json) {
+      {
+        handle: 'contact123',
+        name: 'Contact',
+        organization: 'Organization',
+        street: 'Street',
+        street2:  'Street 2',
+        street3:  'Street 3',
+        city: 'City',
+        state:  'State',
+        postal_code:  1234,
+        country_code: 'PH',
+        local_name: 'Local Contact',
+        local_organization: 'Local Organization',
+        local_street: 'Local Street',
+        local_street2:  'Local Street 2',
+        local_street3:  'Local Street 3',
+        local_city: 'Local City',
+        local_state:  'Local State',
+        local_postal_code:  1235,
+        local_country_code: 'RP',
+        voice:  '+63.1234567',
+        voice_ext:  1234,
+        fax:  '+63.1234568',
+        fax_ext:  1235,
+        email:  'contact@test.ph'
+      }
+    }
+
+    it { is_expected.to eql expected_json }
+  end
+
   describe '#save' do
     let(:client)  { double('client') }
 
