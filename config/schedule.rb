742195ab8c26226404912b7b7b9a8201846fbb22
Ealden Esto E. Escañan <ealden@dot.ph>
2016-08-01 14:50:43 +0800

    Run every 30 seconds

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,5 @@ job_type :delayed_rake, 'sleep :delay && cd :path && :environment_variable=:envi
 
 every 1.minute, roles: [:app] do
   rake 'registry:sync'
-  delayed_rake 'registry:sync', delay: 15
   delayed_rake 'registry:sync', delay: 30
-  delayed_rake 'registry:sync', delay: 45
 end
