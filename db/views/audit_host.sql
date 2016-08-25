69cdee42f97c8ea0aa126fcccbe8b0a94ce037f0
kian1213 <chrono0424@gmail.com>
2016-08-25 17:14:50 +0800

    add audit_host sql view

---
+++ db/views/audit_host.sql
@@ -0,0 +1,29 @@
+DROP VIEW IF EXISTS audit_host;
+DROP TABLE IF EXISTS audit_host;
+
+CREATE VIEW audit_host AS
+SELECT
+  audit_transaction,
+  audit_operation,
+  roid,
+  nam,
+  clid,
+  crid,
+  createdate
+FROM dblink('dbname=registry user=coccauser password=coccauser', '
+  SELECT
+    audit_transaction,
+    audit_operation,
+    host_name,
+    ip,
+    address
+  FROM audit.host
+') cocca (
+  audit_transaction BIGINT,
+  audit_operation   VARCHAR(1),
+  roid              VARCHAR(89),
+  name              VARCHAR(255),
+  clid              VARCHAR(16),
+  crid              VARCHAR(16),
+  createdate        TIMESTAMP
+);
