7e7d70d9ec1a87fb2ccf56f5727a184687d933e4
kian1213 <chrono0424@gmail.com>
2016-09-07 15:28:49 +0800

    support bulk create and delete of domain_hosts from troy

--- app/controllers/domain_hosts_controller.rb
+++ app/controllers/domain_hosts_controller.rb
@@ -1,21 +1,25 @@
 class DomainHostsController < SecureController
   def create
-    domain_host = DomainHost.new  partner: current_partner,
-                                  domain: params[:domain_id],
-                                  name:   params[:name]
-
-    unless params[:old_name].nil?
-      old_domain_host = DomainHost.new  partner:  current_partner,
-                                        domain:   params[:domain_id],
-                                        name:     params[:old_name]
-
-      if old_domain_host.destroy
-        create_domain_host domain_host
-      else
-        head :unprocessable_entity
-      end
+    unless params[:bulk].nil?
+      create_delete_bulk
     else
-      create_domain_host domain_host
+      domain_host = DomainHost.new  partner: current_partner,
+                                    domain: params[:domain_id],
+                                    name:   params[:name]
+
+      unless params[:old_name].nil?
+        old_domain_host = DomainHost.new  partner:  current_partner,
+                                          domain:   params[:domain_id],
+                                          name:     params[:old_name]
+
+        if old_domain_host.destroy
+          create_domain_host domain_host
+        else
+          head :unprocessable_entity
+        end
+      else
+        create_domain_host domain_host
+      end
     end
   end
 
@@ -26,6 +30,24 @@ class DomainHostsController < SecureController
 
     if domain_host.destroy
       render json: domain_host
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  def create_delete_bulk
+    new_list    = params[:domain_host_add].join(',')
+    delete_list = params[:domain_host_delete].join(',')
+    new_domain_host = DomainHost.new  partner: current_partner,
+                                      domain: params[:domain_id],
+                                      name: new_list
+
+    remove_domain_host = DomainHost.new  partner: current_partner,
+                                      domain: params[:domain_id],
+                                      name: delete_list
+
+    if remove_domain_host.destroy
+      create_domain_host new_domain_host
     else
       head :unprocessable_entity
     end
