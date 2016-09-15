b62a3852c3ff85bac3ac0c706262aab67b58f924
kian1213 <chrono0424@gmail.com>
2016-09-15 13:47:48 +0800

    check domain host availability before delete and create

--- app/models/domain_host.rb
+++ app/models/domain_host.rb
@@ -7,13 +7,28 @@ class DomainHost < EPP::Model
   def create
     return false unless valid?
 
-    client.update(add_command).success?
+    domain_host_for_add = self.check_existing_host
+
+    unless domain_host_for_add.blank?
+      new_domain_host_for_add = self.name.split(',') - domain_host_for_add
+      self.name = new_domain_host_for_add.join(',')
+      client.update(add_command).success?
+    else
+      client.update(add_command).success?
+    end
   end
 
   def destroy
     return false unless valid?
 
-    client.update(remove_command).success?
+    domain_host_for_delete = self.check_existing_host
+
+    unless domain_host_for_delete.blank?
+      self.name = domain_host_for_delete.join(',')
+      client.update(remove_command).success?
+    else
+      return true
+    end
   end
 
   def as_json options = nil
@@ -37,5 +52,24 @@ class DomainHost < EPP::Model
         ns: self.name.split(',')
       }
     }
+  end
+
+  def check_existing_host
+    existing_hosts = []
+    domain_hosts = self.name.split(',')
+
+    response = client.info EPP::Domain::Info.new self.domain
+    info     = EPP::Domain::InfoResponse.new response
+    current_domain_hosts_info = info.nameservers
+    unless current_domain_hosts_info.nil?
+      current_domain_hosts_array = current_domain_hosts_info.map{|a| a.map{|k,v|v}.join}
+
+      domain_hosts.map{|domain_host|
+        if current_domain_hosts_array.include?(domain_host)
+          existing_hosts << domain_host
+        end
+      }
+    end
+    existing_hosts
   end
 end
