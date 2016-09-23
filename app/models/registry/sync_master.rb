3ab96b287187c96f594beeee513667cd9c45aa3f
Joey Mendoza <jm.mendoza@dot.ph>
2016-09-23 15:52:40 +0800

    Added sync master dependents

--- app/models/registry/sync_master.rb
+++ app/models/registry/sync_master.rb
@@ -1,2 +1,8 @@
 class Registry::SyncMaster < ActiveRecord::Base
+  
+  has_many :domains, foreign_key: :audit_transaction, class_name: Registry::SyncDomain
+  has_many :contacts, foreign_key: :audit_transaction, class_name: Registry::SyncContact
+  has_many :hosts, foreign_key: :audit_transaction, class_name: Registry::SyncHost
+  has_many :host_addresses, foreign_key: :audit_transaction, class_name: Registry::SyncHostAddress
+  has_many :domain_hosts, foreign_key: :audit_transaction, class_name: Registry::SyncDomainHost
 end
