40bfb8cf4f0f91f3b314c83c109515a081827c3a
Joey Mendoza <jm.mendoza@dot.ph>
2016-11-02 16:45:10 +0800

    updated epp client and tester

---
+++ app/models/nz_tester.rb
@@ -0,0 +1,16 @@
+class NzTester
+	def self.client
+		context = OpenSSL::SSL::SSLContext.new
+		context.ca_file = '/home/deploy/ssl/nzrs-root-2012.pem'
+		raw = File.read '/home/deploy/ssl/mysrs.pem'
+		certificate = OpenSSL::X509::Certificate.new raw
+		context.cert = certificate
+		keyfile = File.new('/home/deploy/ssl/nz-api.next.mysrs.co.nz.key')
+		key = OpenSSL::PKey.read(keyfile)
+		context.key = key
+		services = ['urn:ietf:params:xml:ns:domain-1.0', 'urn:ietf:params:xml:ns:contact-1.0']
+		extension = ['urn:ietf:params:xml:ns:secDNS-1.1']
+		client = EPP::Client.new('115', 'F3dc40b2E', 'srstestepp.srs.net.nz', {:ssl_context => context, :extensions => extension, :services => services, :address_family => 'AF_INET'})
+		client
+	end
+end
