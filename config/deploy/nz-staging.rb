4cd6b126d4b68b4533b87c2bcad58f95d4d34dbd
Joey Mendoza <jm.mendoza@dot.ph>
2016-11-07 19:35:18 +0800

    Renamed nz to nz-api

--- config/deploy/nz-staging.rb
+++ config/deploy/nz-staging.rb
@@ -1,3 +1,3 @@
-role :app, %w{deploy@nz.staging.local}
-role :db,  %w{deploy@nz-db.staging.local}
+role :app, %w{deploy@nz-api.staging.local}
+role :db,  %w{deploy@nz-api-db.staging.local}
 set :branch, 'nz-api'
